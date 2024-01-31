a07b565952a9c85b31557856e7f2177b6160216a
CJ484 <ccoreas484@gmail.com>
2024-01-30 22:16:04 -0500

    Add new image assets

---
+++ app/assets/Images/index.js
@@ -0,0 +1,9 @@
+import banner from './banner.png';
+import logo from './logo.png';
+import searchSymbol from './search.svg';
+import Heart from './heart.svg';
+import User from './user.svg';
+import sun from './sun.svg';
+import moon from './moon.svg';
+
+export {banner, logo, searchSymbol, Heart, User, sun, moon}
