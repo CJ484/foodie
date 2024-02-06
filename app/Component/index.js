c42e3ec47bb6250cd604485cc69125da5b6825ac
CJ484 <ccoreas484@gmail.com>
2024-02-06 16:47:04 -0500

    added Nav to the index file of components

--- app/Component/index.js
+++ app/Component/index.js
@@ -1,5 +1,6 @@
 import LogInForm from './LogInForm';
 import RegisterForm from './RegisterForm';
 import FoodCard from './FoodCard';
+import Nav from './Nav';
 
-export {LogInForm, RegisterForm, FoodCard};
+export {LogInForm, RegisterForm, FoodCard, Nav};
