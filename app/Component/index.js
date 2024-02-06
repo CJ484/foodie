0584fcc4fd5af3753fdcf5656cc389c7054c4b7d
CJ484 <ccoreas484@gmail.com>
2024-02-06 17:45:52 -0500

    Add PageSelector component to Component index.js

--- app/Component/index.js
+++ app/Component/index.js
@@ -2,5 +2,6 @@ import LogInForm from './LogInForm';
 import RegisterForm from './RegisterForm';
 import FoodCard from './FoodCard';
 import Nav from './Nav';
+import PageSelector from './PageSelector';
 
-export {LogInForm, RegisterForm, FoodCard, Nav};
+export {LogInForm, RegisterForm, FoodCard, Nav, PageSelector};
