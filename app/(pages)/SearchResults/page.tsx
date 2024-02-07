989cfdf28878255549528e034aece8c3215d9252
CJ484 <ccoreas484@gmail.com>
2024-02-06 21:55:00 -0500

    Refactor search results page component

--- app/(pages)/SearchResults/page.tsx
+++ app/(pages)/SearchResults/page.tsx
@@ -1,18 +1,64 @@
-import { useParams } from "next/navigation";
-import Link from "next/link";
-const SearchResults = (data: any) => {
-  const { searchQuery } = useParams<{ searchQuery: string }>();
+"use client";
+import React, { useState, useEffect } from "react";
+import { FoodCard, Nav, PageSelector } from "../../Component";
+import { ToastContainer } from "react-toastify";
+import { useSearchParams } from "next/navigation";
+import SpoonRecipes from "../../api/SpoonRecipes";
+import "react-toastify/dist/ReactToastify.css";
+import styles from "../../assets/styles/pages/searchResults.module.scss";
+const SearchResults = () => {
+  const searchParams = useSearchParams();
+  const queryResults = searchParams.get("query");
+
+  const [foodData, setFoodData] = useState([] as any);
+  const [pageCountLimit, setPageCountLimit] = useState(0);
+  const [resultsOffSet, setResultsOffSet] = useState(0);
+
+  
+  const searchFoodApi = async () => {
+    await SpoonRecipes({
+      incomingData: queryResults,
+      resultOffset: resultsOffSet,
+    }).then((results) => {
+      setPaginationLimit(results.data.totalResults);
+      setFoodIncomingData(results.data.results);
+    });
+  };
+
+  const setPaginationLimit = (foodCount: number) => {
+    const pages = Math.ceil((foodCount / 10)) - 12;
+
+    setPageCountLimit(pages);
+  };
+
+  const setFoodIncomingData = (input: any) => {
+    const spoonApiData = input.map((data: any) => {
+      const { title, image, id } = data;
+      return { title: title, image: image, id: id };
+    });
+    setFoodData(spoonApiData);
+  };
+
+  useEffect(() => {
+    searchFoodApi();
+    console.log(resultsOffSet);
+    
+  }, [resultsOffSet]);
+  
 
   return (
-    <div>
-      <h1>Search Results</h1>
-      <ul>
-        {data?.map((item: any) => (
-          <li key={item.id}>
-            <Link href={`/item/${item.id}`}>{item.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.searchResultsPage}>
+      <Nav />
+      <h1 className={styles.searchQuery}>
+        Search Results for &quot;{queryResults}&quot;
+      </h1>
+      <div className={styles.foodResults}>
+        {foodData.map((data: any, index: number) => {
+          return <FoodCard key={index} data={data} />;
+        })}
+      </div>
+      <PageSelector setResultsOffSet={setResultsOffSet} pageCount={pageCountLimit} />
+      <ToastContainer />
     </div>
   );
 };
