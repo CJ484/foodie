aa66bc00d4dbb066595360cc00787d216ed38319
CJ484 <ccoreas484@gmail.com>
2024-02-06 20:19:30 -0500

    Add search functionality to desktop navigation

--- app/Component/Nav/Desktop/index.tsx
+++ app/Component/Nav/Desktop/index.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import { logo, searchSymbol, User, Heart, sun, moon } from "../../../assets/Images";
-import styles from "../../../assets/styles/components/nav.module.scss";
 import {
-  InputAdornment,
-  TextField,
-		Switch
-} from "@mui/material";
+  logo,
+  searchSymbol,
+  User,
+  Heart,
+  sun,
+  moon,
+} from "../../../assets/Images";
+import styles from "../../../assets/styles/components/nav.module.scss";
+import { InputAdornment, TextField, Switch } from "@mui/material";
 
 const DesktopNav: React.FC = () => {
+  const router = useRouter();
   const [search, setSearch] = useState("");
+
+  const searchFoodApi = () => {
+    router.push(`/SearchResults?query=${search}`);
+  };
 
   return (
     <div className={styles.desktop_nav}>
@@ -25,11 +34,19 @@ const DesktopNav: React.FC = () => {
           sx={{ color: "white" }}
           aria-label="Search Bar"
           placeholder="Search"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") searchFoodApi();
+          }}
+          onChange={(e) => setSearch(e.target.value)}
           InputProps={{
-											 className: styles.nav_searchBar,
+            className: styles.nav_searchBar,
             endAdornment: (
               <InputAdornment position="end">
-                <Image src={searchSymbol} alt="search symbol" />
+                <Image
+                  onClick={searchFoodApi}
+                  src={searchSymbol}
+                  alt="search symbol"
+                />
               </InputAdornment>
             ),
           }}
@@ -46,15 +63,15 @@ const DesktopNav: React.FC = () => {
                 <Image src={User} alt="Profile" />
               </Link>
             </li>
-												<li>
-													<Image	src={sun} alt="light mode" />
-														<Switch
-																defaultChecked
-																color="default"
-																inputProps={{ "aria-label": "checkbox with default color" }}
-														/>
-													<Image	src={moon} alt="dark mode" />
-												</li>
+            <li>
+              <Image src={sun} alt="light mode" />
+              <Switch
+                defaultChecked
+                color="default"
+                inputProps={{ "aria-label": "checkbox with default color" }}
+              />
+              <Image src={moon} alt="dark mode" />
+            </li>
           </ul>
         </div>
       </div>
