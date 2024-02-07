c3ee25165774fec218a40e22b014047855b80acc
cj484 <ccoreas484@gmail.com>
2024-02-07 13:08:56 -0500

    Update home page with Spline component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 'use client';
 import Image from 'next/image';
+import Spline from '@splinetool/react-spline';
 import Nav from './Component/Nav';
 import {banner} from './assets/Images';
 import styles from './assets/styles/pages/globals.module.scss';
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Nav />
-      <div className={styles.homePage}>
-        <Image src={banner} alt="banner"/>
-        <h1>&quot;Unveiling Flavor:<br/> Where Recipes come to Life&quot;</h1>
-      </div>
-    </main>
-  );
+	return (
+		<main className={styles.main}>
+			<Nav />
+			<div className={styles.homePage}>
+				<Spline className={styles.spline} scene='https://prod.spline.design/YKciEfAnDTH5vI0T/scene.splinecode'></Spline>
+				<h1>
+          &quot;Unveiling Flavor:
+					<br /> Where Recipes come to Life&quot;
+				</h1>
+			</div>
+		</main>
+	);
 }
