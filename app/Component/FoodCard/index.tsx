db81f17727c3d3629e713dfa1188ba99d2300994
CJ484 <ccoreas484@gmail.com>
2024-02-06 21:11:52 -0500

    Refactor FoodCard component and add toast notification

--- app/Component/FoodCard/index.tsx
+++ app/Component/FoodCard/index.tsx
@@ -1,14 +1,32 @@
 import Image from "next/image";
-import {Circles} from 'react-loader-spinner';
+import { Circles } from "react-loader-spinner";
+import { toast } from "react-toastify";
 import styles from "../../assets/styles/components/foodCard.module.scss";
-const FoodCard = (data: any) => {
+const FoodCard = ({ data }: any) => {
+  const notify = () => toast('Added to Favorites!');
+  const addToFavorites = (id: number) => {
+    notify();
+  };
+
   return (
-    <div className={styles.foodCard}>
-        {/* {data ? <Circles /> : <Image src={data.image} alt="Food Image" />} */}
-      <Circles wrapperClass={styles.foodCard__image}/>
+    <div
+      onClick={() => addToFavorites(data.id)}
+      aria-label={data.id}
+      className={styles.foodCard}
+    >
+      {data.image ? (
+        <Image
+          width={600}
+          height={600}
+          className={styles.foodCard__image}
+          src={data.image}
+          alt={`Food Image for ${data.title}`}
+        />
+      ) : (
+        <Circles wrapperClass={styles.foodCard__image} />
+      )}
       <div className={styles.foodCard__content}>
-        <h1 className={styles.foodCard__title}>title</h1>
-        <p className={styles.foodCard__description}>description</p>
+        <h1 className={styles.foodCard__title}>{data.title}</h1>
       </div>
     </div>
   );
