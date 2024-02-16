e9c884a6cdafe95a167fd89b55e63eaf9825d6ee
CJ484 <ccoreas484@gmail.com>
2024-02-16 16:20:04 -0500

    Add Spoon Recipes API call and documentation

--- app/api/SpoonRecipes/index.tsx
+++ app/api/SpoonRecipes/index.tsx
@@ -1,4 +1,17 @@
 import axios from "axios";
+
+// ? Get Spoon Recipes
+// * Api call is inputted with the searchInput that corresponds to the recipe
+// * The response will give all information that Spooncular API has on the recipe
+// * Which includes the title, image, and id of the recipe. 
+
+// ? The Api has a list of different parameters that can be used to filter the search
+// ? For this Component, we are using the Complex Search parameter, which will return an object
+// ? with the following properties: 
+// * - id: number
+// * - title: string
+// * - image: string
+// ? We can change the amount of results, the offset, and the query.
 const SpoonRecipes = ({incomingData, resultOffset}: {incomingData: any, resultOffset: number}) => {
   const searchInput = incomingData;
   const pageOffset = resultOffset || 0;
