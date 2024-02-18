7e8fdc6a44cbf381daaa37b06c142b06680d6762
CJ484 <ccoreas484@gmail.com>
2024-02-17 19:55:18 -0500

    Add modal component and update styles in MyList page

--- app/(pages)/MyList/page.tsx
+++ app/(pages)/MyList/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FoodCard, Nav } from "../../Component";
+import { Modal } from "@mui/material";
 import SpoonRecipes from "../../api/SpoonRecipes";
 import styles from "../../assets/styles/pages/myList.module.scss";
 
@@ -13,6 +14,8 @@ const MyList = () => {
   ];
 
   const [foods, setFoods] = useState([] as any);
+  const [modalOpen, setModalOpen] = useState(false);
+
 
   const getFavoriteRecipes = async () => {
     const favoriteRecipes = await Promise.all(
@@ -29,22 +32,31 @@ const MyList = () => {
     });
     setFoods(favoriteRecipes);
   };
-
-  getFavoriteRecipes();
+  
+  useEffect(() => {
+    getFavoriteRecipes();
+  }, []);
 
   return (
     <div className={styles.myListPage}>
       <Nav />
       <h1 className={styles.myListPage_title}>My List</h1>
       <div className={styles.myListPage_recipes}>
-        {foods.map((food: any) => {
+        {foods.map((food: any, index: number) => {
           return (
             <>
-              <FoodCard data={food}/>
+              <FoodCard setModalOpen={setModalOpen} key={index} data={food} />
             </>
           );
         })}
       </div>
+      <Modal
+        className={styles.modal}
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+      >
+        <h1>Recipe Title</h1>
+      </Modal>
     </div>
   );
 };
