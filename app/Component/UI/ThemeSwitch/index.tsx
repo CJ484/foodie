b794eee22b42d0a9bd6dd41d615d7e0099912466
CJ484 <ccoreas484@gmail.com>
2024-02-18 19:07:35 -0500

    removed junk code

--- app/Component/UI/ThemeSwitch/index.tsx
+++ app/Component/UI/ThemeSwitch/index.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import {Switch} from "@mui/material";
 import {sun, moon} from "../../../assets/Images";
-import styles from '../../assets/styles/components/themeSwitch.module.scss';
+import styles from '../../../assets/styles/components/themeSwitch.module.scss';
 
 const ThemeSwitch = () => {
 
   // ? Theme State
   // * This state will store the current theme of the application.
   // * The 'Active' state will store the current state of the theme switch.
-  const [theme, setTheme] = React.useState("light");
   const [active, setActive] = React.useState(false);
 
   // ? Toggle Theme
@@ -20,32 +19,16 @@ const ThemeSwitch = () => {
     const theme = localStorage.getItem("theme");
     if (theme === "dark") {
       localStorage.setItem("theme", "light");
-      setTheme("light");
       document.documentElement.classList.remove("globals_dark__r55TQ");
       setActive(false);
       return true;
     } else {
       localStorage.setItem("theme", "dark");
-      setTheme("dark");
       document.documentElement.classList.add("globals_dark__r55TQ");
       setActive(true);
       return false;
     }
   };
-
-  // ? Set Theme
-  // * This function will set the theme to the user's preference
-  // * if the user has already set a theme in local storage.
-  // * It will also set the theme to light mode if the user has not
-  // * set a theme in local storage.
-  useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
-    if (theme === localTheme) {
-      console.log("theme is already set to", localTheme);
-      
-    }
-  }
-  , []);
 
     return (
       <div className={styles.themeSwitch}>
