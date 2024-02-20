7e10a8f18cee0af9879dbffbe625bbcd83528a80
CJ484 <ccoreas484@gmail.com>
2024-02-20 11:48:12 -0500

    Refactor UsersFavoriteFood API to use axios and environment variables

--- app/api/UsersFavoriteFood/index.tsx
+++ app/api/UsersFavoriteFood/index.tsx
@@ -1,12 +1,15 @@
-import SpoonRecipes from "../SpoonRecipes";
+import axios from "axios";
 const UsersFavoriteFood = async ({data}: {data: any}) => {
+  const apiKey = process.env.NEXT_PUBLIC_API_SPOONCULAR_KEY;
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
   return await Promise.all(
     data.map((recipeId: string) =>
-      SpoonRecipes({ incomingData: recipeId, resultOffset: 0 })
+      axios.get(`${apiUrl}/recipes/${recipeId}/information?apiKey=${apiKey}`)
     )
   ).then((response) => {
     const foodData = response.map((food: any) => {
-      const { id, title, image } = food.data.results[0];
+      const { id, title, image } = food.data;
       return { id, title, image };
     });
 
